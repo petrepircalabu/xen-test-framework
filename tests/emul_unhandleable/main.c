ff2cf9697a64823bde66c3845dfa110a740f0c00
Petre Pircalabu <ppircalabu@bitdefender.com>
2017-07-18 15:17:28 +0300

    SQUASME: emul_unhandleable monitor test stub
    Signed-off-by: Petre Pircalabu <ppircalabu@bitdefender.com>

---
+++ tests/emul_unhandleable/main.c
@@ -0,0 +1,28 @@
+/**
+ * @file tests/emul_unhandleable/main.c
+ * @ref test-emul_unhandleable
+ *
+ * @page test-emul_unhandleable emul_unhandleable
+ *
+ * @todo Docs for test-emul_unhandleable
+ *
+ * @see tests/emul_unhandleable/main.c
+ */
+#include <xtf.h>
+
+const char test_title[] = "Test emul_unhandleable";
+
+void test_main(void)
+{
+    xtf_success(NULL);
+}
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
