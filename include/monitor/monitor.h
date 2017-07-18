3034c21b4ab31b99a9683709cbea8f164586cb54
Petre Pircalabu <ppircalabu@bitdefender.com>
2017-07-18 15:17:28 +0300

    emul_unhandleable monitor test
    TODO:
     - FIX xtf_evtchn_loop exit condition to take into account the guest
     exit
     - monitor arguments should be defined per-test. (requirement: arguments
     should use templates to resolve at run-time various identifiers (e.g.
     vm_name - $(xl domid {{vm_name}} )
     - refactor instruction replacement procedure (read the instruction size
     and take into account)
    
    Signed-off-by: Petre Pircalabu <ppircalabu@bitdefender.com>

---
+++ include/monitor/monitor.h
@@ -0,0 +1,97 @@
+/*
+ * XTF Monitor interface
+ */
+
+#ifndef XTF_MONITOR_H
+#define XTF_MONITOR_H
+
+#include <inttypes.h>
+#include <xenctrl.h>
+#include <xenevtchn.h>
+#include <xen/vm_event.h>
+
+typedef enum
+{
+    XTF_MON_LEVEL_FATAL,
+    XTF_MON_LEVEL_ERROR,
+    XTF_MON_LEVEL_WARNING,
+    XTF_MON_LEVEL_INFO,
+    XTF_MON_LEVEL_DEBUG,
+    XTF_MON_LEVEL_TRACE,
+} xtf_mon_log_level_t;
+
+void xtf_log(xtf_mon_log_level_t lvl, const char *fmt, ...) __attribute__((__format__(__printf__, 2, 3)));
+
+#define XTF_MON_FATAL(format...)    xtf_log(XTF_MON_LEVEL_FATAL,    format)
+#define XTF_MON_ERROR(format...)    xtf_log(XTF_MON_LEVEL_ERROR,    format)
+#define XTF_MON_WARNING(format...)  xtf_log(XTF_MON_LEVEL_WARNING,  format)
+#define XTF_MON_INFO(format...)     xtf_log(XTF_MON_LEVEL_INFO,     format)
+#define XTF_MON_DEBUG(format...)    xtf_log(XTF_MON_LEVEL_DEBUG,    format)
+#define XTF_MON_TRACE(format...)    xtf_log(XTF_MON_LEVEL_TRACE,    format)
+
+typedef struct xtf_evtchn_ops
+{
+    int (*mem_access_handler)(domid_t domain_id, vm_event_request_t *req, vm_event_response_t *rsp);
+    int (*singlestep_handler)(domid_t domain_id, vm_event_request_t *req, vm_event_response_t *rsp);
+    int (*emul_unimpl_handler)(domid_t domain_id, vm_event_request_t *req, vm_event_response_t *rsp);
+} xtf_evtchn_ops_t;
+
+/** XTF Event channel interface */
+typedef struct xtf_evtchn
+{
+    xenevtchn_handle *xce_handle;           /**< Event channel handle */
+    xenevtchn_port_or_error_t remote_port;  /**< Event channel remote port */
+    evtchn_port_t local_port;               /**< Event channel local port */
+    vm_event_back_ring_t back_ring;         /**< vm_event back ring */
+    void *ring_page;                        /**< Shared ring page */
+    xtf_evtchn_ops_t ops;                  /**< Test specific event callbacks */
+} xtf_evtchn_t;
+
+int add_evtchn(xtf_evtchn_t *evt, domid_t domain_id);
+xtf_evtchn_t *get_evtchn(domid_t domain_id);
+#define evtchn(domain_id) ( get_evtchn(domain_id) )
+
+/** XTF Monitor Driver */
+typedef struct xtf_monitor
+{
+    xc_interface *xch;                      /**< Control interface */
+    xtf_evtchn_t *evt;                      /**< Event channel list */
+    xtf_mon_log_level_t log_lvl;            /**< Log Level */
+    int (*setup)(int, char*[]);             /**< Test specific setup */
+    int (*init)(void);                      /**< Test specific initialization */
+    int (*run)(void);                       /**< Test specific routine */
+    int (*cleanup)(void);                   /**< Test specific cleanup */
+} xtf_monitor_t;
+
+xtf_monitor_t *get_monitor();
+#define monitor ( get_monitor() )
+#define xtf_xch ( monitor->xch )
+
+#define call_helper(func, ... )         ( (func) ? func(__VA_ARGS__) : 0 )
+#define xtf_monitor_setup(argc, argv)   ( call_helper(monitor->setup, argc, argv) )
+#define xtf_monitor_init()              ( call_helper(monitor->init) )
+#define xtf_monitor_run()               ( call_helper(monitor->run) )
+#define xtf_monitor_cleanup()           ( call_helper(monitor->cleanup) )
+
+int xtf_evtchn_init(domid_t domain_id);
+int xtf_evtchn_cleanup(domid_t domain_id);
+int xtf_evtchn_loop(domid_t domain_id);
+
+extern const char monitor_test_help[];
+
+void usage();
+
+#define XTF_MONITOR(pname) \
+extern xtf_monitor_t *xtf_monitor_instance = &pname;
+
+#endif /* XTF_MONITOR_H */
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
---
+++ tests/emul_unimpl/monitor.c
@@ -0,0 +1,320 @@
+/**
+ * @file tests/emul_unimpl/monitor.c
+ */
+
+#include <errno.h>
+#include <getopt.h>
+#include <inttypes.h>
+#include <monitor.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <unistd.h>
+
+typedef struct emul_unimpl_monitor
+{
+    xtf_monitor_t mon;
+    domid_t domain_id;
+    uint64_t address;
+    uint16_t altp2m_view_id;
+    void *map;
+    unsigned long gfn;
+    unsigned char instr[8];
+    unsigned long instr_size;
+} emul_unimpl_monitor_t;
+
+const char monitor_test_help[] = \
+    "Usage: test-monitor-emul_unimpl [options] <domid>\n"
+    "\t -a <address>: the address where an invalid instruction will be injected\n"
+    ;
+
+static int emul_unimpl_setup(int argc, char *argv[]);
+static int emul_unimpl_init();
+static int emul_unimpl_run();
+static int emul_unimpl_cleanup();
+
+static emul_unimpl_monitor_t monitor_instance =
+{
+    .mon =
+    {
+        .setup      = emul_unimpl_setup,
+        .init       = emul_unimpl_init,
+        .run        = emul_unimpl_run,
+        .cleanup    = emul_unimpl_cleanup,
+    }
+};
+
+static int emul_unimpl_mem_access(domid_t domain_id, vm_event_request_t *req, vm_event_response_t *rsp);
+static int emul_unimpl_singlestep(domid_t domain_id, vm_event_request_t *req, vm_event_response_t *rsp);
+static int emul_unimpl_emul_unimpl(domid_t domain_id, vm_event_request_t *req, vm_event_response_t *rsp);
+
+static xtf_evtchn_t evtchn_instance =
+{
+    .ops = 
+    {
+        .mem_access_handler     = emul_unimpl_mem_access,
+        .singlestep_handler     = emul_unimpl_singlestep,
+        .emul_unimpl_handler    = emul_unimpl_emul_unimpl,
+    }
+};
+
+static int emul_unimpl_setup(int argc, char *argv[])
+{
+    int ret, c;
+    static struct option long_options[] = {
+        {"help",    no_argument,    0,  'h'},
+        {"address", required_argument,    0,  'a'},
+        {0, 0, 0, 0}
+    };
+    emul_unimpl_monitor_t *pmon = (emul_unimpl_monitor_t *)monitor;
+
+    if ( !pmon )
+        return -EINVAL;
+
+    if ( argc == 1 )
+    {
+        usage();
+        return -EINVAL;
+    }
+    while ( 1 )
+    {
+        int option_index = 0;
+        c = getopt_long(argc, argv, "ha:", long_options, &option_index);
+        if ( c == -1 ) break;
+
+        switch ( c )
+        {
+            case 'h':
+                usage();
+                exit(0);
+                break;
+            case 'a':
+                pmon->address = strtoul(optarg, NULL, 0);
+                break;
+            default:
+                XTF_MON_ERROR("%s: Invalid option %s\n", argv[0], optarg);
+                return -EINVAL;
+        }
+
+        if ( !pmon->address )
+        {
+            XTF_MON_ERROR("%s: Please specify a valid instruction injection address\n", argv[0]);
+            return -EINVAL;
+        }
+
+        if ( optind != argc - 1 )
+        {
+            XTF_MON_ERROR("%s: Please specify the domain id\n", argv[0]);
+            return -EINVAL;
+        }
+    }
+
+    pmon->domain_id = atoi(argv[optind]);
+
+    if ( pmon->domain_id <= 0 )
+    {
+        XTF_MON_ERROR("%s: Invalid domain id\n");
+        return -EINVAL;
+    }
+
+    add_evtchn(&evtchn_instance, pmon->domain_id);
+
+    return 0;
+}
+
+static int emul_unimpl_init()
+{
+    int rc = 0;
+    emul_unimpl_monitor_t *pmon = (emul_unimpl_monitor_t *)monitor;
+
+    if ( !pmon )
+        return -EINVAL;
+
+    rc = xtf_evtchn_init(pmon->domain_id);
+    if ( rc < 0 )
+        return rc;
+
+    rc = xc_domain_set_access_required(xtf_xch, pmon->domain_id, 1);
+    if ( rc < 0 )
+    {
+        XTF_MON_ERROR("Error %d setting mem_access listener required\n", rc);
+        return rc;
+    }
+
+    rc = xc_monitor_emul_unimplemented(xtf_xch, pmon->domain_id, 1);
+    if ( rc < 0 )
+    {
+        XTF_MON_ERROR("Error %d emulation unimplemented with vm_event\n", rc);
+        return rc;
+    }
+
+    rc = xc_altp2m_set_domain_state(xtf_xch, pmon->domain_id, 1);
+    if ( rc < 0 )
+    {
+        XTF_MON_ERROR("Error %d enabling altp2m on domain!\n", rc);
+        return rc;
+    }
+
+    rc = xc_altp2m_create_view(xtf_xch, pmon->domain_id, XENMEM_access_rwx,
+            &pmon->altp2m_view_id );
+    if ( rc < 0 )
+    {
+        XTF_MON_ERROR("Error %d creating altp2m view!\n", rc);
+        return rc;
+    }
+
+    pmon->gfn = xc_translate_foreign_address(xtf_xch, pmon->domain_id, 0, pmon->address);
+
+    pmon->map = xc_map_foreign_range(xtf_xch, pmon->domain_id, 4096,
+            PROT_READ | PROT_WRITE , pmon->gfn);
+    if ( !pmon->map )
+    {
+        XTF_MON_ERROR("Failed to map page.\n");
+        return -1;
+    }
+
+    rc = xc_altp2m_set_mem_access(xtf_xch, pmon->domain_id, pmon->altp2m_view_id,
+            pmon->gfn, XENMEM_access_rw);
+    if ( rc < 0 )
+    {
+        XTF_MON_ERROR("Error %d setting altp2m memory access!\n", rc);
+        return rc;
+    }
+
+    rc = xc_altp2m_switch_to_view(xtf_xch, pmon->domain_id, pmon->altp2m_view_id );
+    if ( rc < 0 )
+    {
+        XTF_MON_ERROR("Error %d switching to altp2m view!\n", rc);
+        return rc;
+    }
+
+    rc = xc_monitor_singlestep(xtf_xch, pmon->domain_id, 1 );
+    if ( rc < 0 )
+    {
+        XTF_MON_ERROR("Error %d failed to enable singlestep monitoring!\n", rc);
+        return rc;
+    }
+
+    return 0;
+}
+
+static int emul_unimpl_run()
+{
+    int rc;
+    emul_unimpl_monitor_t *pmon = (emul_unimpl_monitor_t *)monitor;
+
+    if ( !pmon )
+        return -EINVAL;
+
+    rc = xc_domain_unpause(xtf_xch, pmon->domain_id);
+    if ( rc < 0 )
+    {
+        XTF_MON_ERROR("Error unpausing domain.\n");
+        return rc;
+    }
+
+    return xtf_evtchn_loop(pmon->domain_id);
+}
+
+static int emul_unimpl_cleanup()
+{
+    emul_unimpl_monitor_t *pmon = (emul_unimpl_monitor_t *)monitor;
+
+    if ( !pmon )
+        return -EINVAL;
+
+    if (pmon->map)
+    {
+        munmap(pmon->map, 4096);
+    }
+
+    xc_altp2m_switch_to_view(xtf_xch, pmon->domain_id, 0 );
+
+    xc_altp2m_destroy_view(xtf_xch, pmon->domain_id, pmon->altp2m_view_id);
+
+    xc_altp2m_set_domain_state(xtf_xch, pmon->domain_id, 0);
+
+    xc_monitor_singlestep(xtf_xch, pmon->domain_id, 0);
+
+    xtf_evtchn_cleanup(pmon->domain_id);
+
+    return 0;
+}
+
+
+static int emul_unimpl_mem_access(domid_t domain_id, vm_event_request_t *req, vm_event_response_t *rsp)
+{
+    emul_unimpl_monitor_t *pmon = (emul_unimpl_monitor_t *)monitor;
+    volatile unsigned char *p;
+    int i;
+
+    if (!pmon)
+        return -EINVAL;
+
+    rsp->flags |= VM_EVENT_FLAG_EMULATE | VM_EVENT_FLAG_TOGGLE_SINGLESTEP;
+
+    p = (volatile unsigned char*)(pmon->map + req->u.mem_access.offset);
+
+    pmon->instr[0] = *(p + 0);
+    pmon->instr[1] = *(p + 1);
+    pmon->instr[2] = *(p + 2);
+    pmon->instr[3] = *(p + 3);
+    pmon->instr[4] = *(p + 4);
+    *(p + 0) = 0xD9;
+    *(p + 1) = 0x20;
+    *(p + 2) = 0x00;
+    *(p + 3) = 0x00;
+    *(p + 4) = 0x00;
+    pmon->instr_size = 5;
+
+    rsp->u.mem_access = req->u.mem_access;
+    return 0;
+}
+
+static int emul_unimpl_singlestep(domid_t domain_id, vm_event_request_t *req, vm_event_response_t *rsp)
+{
+    emul_unimpl_monitor_t *pmon = (emul_unimpl_monitor_t *)monitor;
+
+    if (!pmon)
+        return -EINVAL;
+
+    rsp->flags |= VM_EVENT_FLAG_ALTERNATE_P2M | VM_EVENT_FLAG_TOGGLE_SINGLESTEP;
+    rsp->altp2m_idx = pmon->altp2m_view_id;
+
+    /* Restore the execute rights on the test page. */
+    xc_altp2m_set_mem_access(xtf_xch, pmon->domain_id, pmon->altp2m_view_id,
+        pmon->gfn, XENMEM_access_rwx);
+    return 0;
+}
+
+static int emul_unimpl_emul_unimpl(domid_t domain_id, vm_event_request_t *req, vm_event_response_t *rsp)
+{
+    emul_unimpl_monitor_t *pmon = (emul_unimpl_monitor_t *)monitor;
+    volatile unsigned char *p;
+    int i;
+
+    if (!pmon)
+        return -EINVAL;
+
+    p = (volatile unsigned char *)(pmon->map + req->u.mem_access.offset);
+
+    for (i = 0; i < pmon->instr_size; i++)
+        *(p + i) = pmon->instr[i];
+
+    rsp->flags |= VM_EVENT_FLAG_ALTERNATE_P2M ;
+    rsp->altp2m_idx = 0;
+
+    return 0;
+}
+
+XTF_MONITOR(monitor_instance);
+
+/*
+ * Local variables:
+ * mode: C
+ * c-file-style: "BSD"
+ * c-basic-offset: 4
+ * tab-width: 4
+ * indent-tabs-mode: nil
+ * End:
+ */
