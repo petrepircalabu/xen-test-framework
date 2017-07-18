f4b938ea2d9fffe8437fd2226da2889a5bd80288
Petre Pircalabu <ppircalabu@bitdefender.com>
2017-07-18 15:17:28 +0300

    xtf: Add monitor test class
    This class starts alongside the domain a monitor application which opens
    an event channel corresponding to that domain and handles the received
    requests.
    Use the "monitor_args" key to pass test specific arguments to the
    monitor application.
    The arguments will be added in the test's Makefile using the
    TEST-EXTRA-INFO variable.
    
    Signed-off-by: Petre Pircalabu <ppircalabu@bitdefender.com>

---
+++ xtf/monitor_test.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" Monitor test classes.
+
+    The monitor test spawns an test monitor (event channel handler application)
+    instance and runs a DomU image which interacts with it.
+"""
+
+import os.path as path
+from subprocess import Popen, PIPE
+
+from xtf.exceptions import RunnerError
+from xtf.domu_test import DomuTestInstance, DomuTestInfo
+from xtf.logger import Logger
+from xtf.test import TestInstance
+from xtf.xl_domu import XLDomU
+
+class MonitorTestInstance(DomuTestInstance):
+    """Monitor test instance"""
+
+    def __init__(self, env, name, variation, test_info):
+        super(MonitorTestInstance, self).__init__(env, name, variation,
+                                                  test_info)
+
+        self.monitor_args = (test_info.extra["monitor_args"]
+                             .replace("@@VM_PATH@@", self.vm_path())
+        )
+
+    def vm_path(self):
+        """ Return the VM path. """
+        return path.join("tests", self.name, repr(self))
+
+    def monitor_path(self):
+        """ Return the path to the test's monitor app if applicable. """
+        return path.join("tests", self.name, "test-monitor-" + self.name)
+
+    def start_monitor(self, dom_id):
+        """ Starts the monitor application. """
+        cmd = [" ".join([self.monitor_path(), self.monitor_args, str(dom_id)])]
+        Logger().log("Executing '%s'" % (" ".join(cmd), ))
+        return Popen(cmd, shell=True)
+
+    def run(self, opts):
+        # set up the domain
+        domu = XLDomU(self.cfg_path())
+        domu.create()
+        console = domu.console()
+
+        # start the monitor & domain
+        monitor = self.start_monitor(domu.dom_id)
+        # domu.unpause()
+        value, _ = console.wait(self.result_pattern())
+        if value is None:
+            value = "CRASH"
+
+        if monitor.returncode:
+            raise RunnerError("Failed to start monitor application")
+
+        Logger().log(_)
+
+        return value
+
+class MonitorTestInfo(DomuTestInfo):
+    """Monitor test info"""
+
+    def __init__(self, test_json):
+        super(MonitorTestInfo, self).__init__(test_json)
+        self.instance_class = MonitorTestInstance
