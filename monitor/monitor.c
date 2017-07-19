fa211bc5f132ab88c2889b02ae476bfdbfe6067c
Petre Pircalabu <ppircalabu@bitdefender.com>
2017-07-19 20:30:34 +0300

    SQUASME: 19-07-2017 - v2
    Signed-off-by: Petre Pircalabu <ppircalabu@bitdefender.com>

--- monitor/monitor.c
+++ monitor/monitor.c
@@ -1,22 +1,84 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <sys/mman.h>
 #include <xenctrl.h>
 #include <xenevtchn.h>
 
-int main(int argc, char* argv[])
+typedef struct xen_monitor_test
 {
-    domid_t domain_id;
     xc_interface *xch;
 
-    domain_id = atoi(argv[0]);
+    domid_t domain_id;
 
-    xch = xc_interface_open(NULL, NULL, 0);
-    if ( !xch )
+    void *ring_page;
+
+    uint32_t evtchn_port;
+
+} xen_monitor_test_t;
+
+static int monitor_init(xen_monitor_test_t *test, domid_t domain_id)
+{
+    test->xch = xc_interface_open(NULL, NULL, 0);
+    if ( !test->xch )
     {
         printf("Error initialising xenaccess\n");
-        return 1;
+        return -1;
     }
 
+    test->domain_id = domain_id;
+
+    test->ring_page = xc_monitor_enable(test->xch, test->domain_id,
+            &test->evtchn_port);
+    if ( !test->ring_page )
+    {
+        printf("Error enabling monitor\n");
+        return -1;
+    }
+
+    return 0;
+}
+
+static int monitor_cleanup(xen_monitor_test_t *test)
+{
+    int rc;
+
+    if ( test->ring_page )
+        munmap(test->ring_page, XC_PAGE_SIZE);
+    
+    if ( test->xch )
+    {
+        rc = xc_interface_close(test->xch);
+        if ( rc != 0 )
+        {
+            printf("Error closing connection to XEN.\n");
+            return rc;
+        }
+    }
+
+    return 0;
+}
+
+int main(int argc, char* argv[])
+{
+
+    domid_t domain_id;
+
+    xen_monitor_test_t *test = calloc(1, sizeof(xen_monitor_test_t));
+    if (!test)
+    {
+        printf("Failed to allocate memory\n");
+        return -1;
+    }
+
+    domain_id = atoi(argv[1]);
+    printf("Domain ID=%d\n", domain_id);
+
+    monitor_init(test, domain_id);
+
+cleanup:
+    monitor_cleanup(test);
+
+    free(test);
     return 0;
 }
