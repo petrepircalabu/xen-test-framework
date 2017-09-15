d633e3f8a821975be140cdc5fee5a8b85c31bc4d
Petre Pircalabu <ppircalabu@bitdefender.com>
2017-09-15 17:08:40 +0300

    SQUASHME: hvmop-altp2m-set-mem-access-mult: set access and switch to view
    Signed-off-by: Petre Pircalabu <ppircalabu@bitdefender.com>

--- tests/hvmop-altp2m-set-mem-access-multi/main.c
+++ tests/hvmop-altp2m-set-mem-access-multi/main.c
@@ -16,19 +16,50 @@ static uint8_t test_page1[PAGE_SIZE] __page_aligned_bss;
 static uint8_t test_page2[PAGE_SIZE] __page_aligned_bss;
 static uint8_t test_page3[PAGE_SIZE] __page_aligned_bss;
 
+/** Print expected information in the case of an unexpected exception. */
+bool unhandled_exception(struct cpu_regs *regs)
+{
+    printk("Unhandled Exception at %p\n", _p(regs));
+
+    return false;
+}
+
 void test_main(void)
 {
     int rc;
-    xenmem_access_t default_access = XENMEM_access_r;
+    xenmem_access_t default_access = XENMEM_access_rwx;
+    xenmem_access_t access[3] = {
+        XENMEM_access_r,
+        XENMEM_access_r,
+        XENMEM_access_r,
+    };
     uint16_t view_id = 0;
+    uint64_t pages[3];
 
     rc = hvm_altp2m_set_domain_state(true);
     if ( rc < 0 )
         return xtf_error("Error %d enabling altp2m on domain!\n", rc);
 
+    pages[0] = virt_to_gfn(test_page1);
+    pages[1] = virt_to_gfn(test_page2);
+    pages[2] = virt_to_gfn(test_page3);
+
+    /* Setup.  Hook unhandled exceptions for debugging purposes. */
+    xtf_unhandled_exception_hook = unhandled_exception;
+
     rc = hvm_altp2m_create_view(default_access, &view_id);
     if ( rc < 0 )
         return xtf_error("Error %d creating altp2m view!\n", rc);
+
+    rc = hvm_altp2m_set_mem_access_multi(view_id, access, pages, 3);
+    if ( rc < 0 )
+        return xtf_error("Error %d setting altp2m access!\n", rc);
+
+    printk("Switch to view %d\n", view_id);
+
+    rc = hvm_altp2m_switch_to_view(view_id);
+    if ( rc < 0 )
+        return xtf_error("Error %d switing altp2m view!\n", rc);
 
     test_page1[0] = 1;
     test_page2[0] = 1;
