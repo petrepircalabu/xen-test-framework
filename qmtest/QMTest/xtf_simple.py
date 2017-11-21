1b704887651c1ed04b92c2de72d6cd92013769f8
Petre Pircalabu <ppircalabu@bitdefender.com>
2017-11-21 23:31:06 +0200

    SQUASHME: remove xl_domu_resource
    Signed-off-by: Petre Pircalabu <ppircalabu@bitdefender.com>

--- qmtest/QMTest/xtf_simple.py
+++ qmtest/QMTest/xtf_simple.py
@@ -2,8 +2,12 @@
 ########################################################################
 # Imports
 ########################################################################
+import os.path
 
+from   qm.fields import TextField
 from   qm.test.test import Test
+from   xl_domu import XLDomU
+from   xtf_utils import XTFError
 
 ########################################################################
 # Classes
@@ -12,7 +16,30 @@ from   qm.test.test import Test
 class XTFSimpleTest(Test):
     """XTF Simple Test."""
 
+    arguments = [
+        TextField(
+            name="conf_file",
+            title="XL configuration file",
+        ),
+    ]
+
     def Run(self, context, result):
         """Run the test."""
-        domu = context["domu_object"]
-        domu.unpause()
+
+        xtf_root = context.get("XTF_ROOT")
+        if xtf_root is None:
+            context["XTF_ROOT"] = os.getcwd()
+
+        conf = os.path.normpath(os.path.join(context["XTF_ROOT"],
+            self.conf_file))
+        if not os.path.isfile(conf):
+            result.Fail("Invalid XL configuration file.", {"file": conf})
+            return
+
+        try:
+            domu = XLDomU(conf)
+            domu.create()
+            domu.destroy()
+        except XTFError as e:
+            e.Annotate(result)
+            result.Fail("Error while executing test")
